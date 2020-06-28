da5884a0f1f25aacf43a642f78dababe785d339b
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-06-28 09:45:29 +0900

    Create firebase config files

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCAqbH90kZePlxPPySTI6vwyXeL2hajtqg",
+  authDomain: "ec-app-77c3a.firebaseapp.com",
+  databaseURL: "https://ec-app-77c3a.firebaseio.com",
+  projectId: "ec-app-77c3a",
+  storageBucket: "ec-app-77c3a.appspot.com",
+  messagingSenderId: "991652602122",
+  appId: "1:991652602122:web:e9fcac5cc5a028f7c39af3",
+  measurementId: "G-2VL36FEE1K"
+};
