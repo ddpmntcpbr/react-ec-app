917dd258431f404edbcf027a3d97f125f944f064
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-07-20 22:08:29 +0900

    Add fetchProducts in ProductList

--- src/templates/ProductList.jsx
+++ src/templates/ProductList.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React,{useEffect} from "react";
 import {ProductCard} from "../components/Products";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {fetchProducts} from "../reducks/products/operations"
+import {getProducts} from "../reducks/products/selectors"
 
 const ProductList = () => {
   const dispatch = useDispatch();
+  const selector = useSelector((state) => state)
+  const products = getProducts(selector)
 
   useEffect (() => {
     dispatch(fetchProducts())
@@ -12,7 +16,11 @@ const ProductList = () => {
   return (
     <section className="c-section-wrapin">
       <div className="p-grid__row">
-
+        {products.length > 0 &&(
+          products.map(product => (
+            <ProductCard key={product.id} />
+          ))
+        )}
       </div>
     </section>
   )
