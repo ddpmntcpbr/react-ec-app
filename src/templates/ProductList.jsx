a038aa9c8ece16bac08f099a2b3472b090e9af1b
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-07-20 21:43:23 +0900

    Add ProductList as mock-up

---
+++ src/templates/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <section className="c-section-wrapin">
+      <div className="p-grid__row">
+
+      </div>
+    </section>
+  )
+}
+
+export default ProductList
