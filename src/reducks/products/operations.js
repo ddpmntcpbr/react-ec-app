8ed423ad4117c11c391a429a9d57a230b7ac9545
ddpmntcpbr <ddpmntcpbr@gmail.com>
2020-07-20 21:57:35 +0900

    Add fetchProduct

--- src/reducks/products/operations.js
+++ src/reducks/products/operations.js
@@ -3,6 +3,20 @@ import { push } from "connected-react-router";
 
 const productsRef = db.collection("products")
 
+export const fetchProduct = () => {
+  return async (dispatch) => {
+    productsRef.orderBy("updated_at","desc").get()
+      .then(snapshots => {
+        const productList = []
+        snapshots.forEach(snapshot => {
+          const product = snapshot.data();
+          productList.push(product)
+        })
+        dispatch(fetchProductsAction(productList));
+      })
+  }
+}
+
 export const saveProduct = (id,name,description,category,gender,images,price,sizes) => {
   return async (dispatch) => {
     const timestamp = FirebaseTimestamp.now()
